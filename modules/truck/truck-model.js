df0a6d03f91f61d269aa0c195fa0a7aea228fdaf
Jeroen van Vugt <jj.vanvugt@student.avans.nl>
2024-06-12 11:35:44 +0200

    Create truck matrix

--- modules/truck/truck-model.js
+++ modules/truck/truck-model.js
@@ -4,6 +4,7 @@ class TruckModel {
   #width;
   #interval;
   #type;
+  #matrix;
 
   constructor(id, length, width, interval, type) {
     this.#id = id;
@@ -11,6 +12,7 @@ class TruckModel {
     this.#width = width;
     this.#interval = interval;
     this.#type = type;
+    this.#matrix = this.#createMatrix(length, width);
   }
 
   getId() { return this.#id; }
@@ -22,6 +24,24 @@ class TruckModel {
   getInterval() { return this.#interval; }
 
   getType() { return this.#type; }
+
+  getMatrix() { return this.#matrix; }
+
+  #createMatrix(length, width) {
+    let array = [];
+
+    for(let i = 0; i < length; ++i) {
+      let rows = [];
+
+      for (let j = 0; j < width; ++j) {
+        rows.push(0);
+      }
+
+      array.push(rows);
+    }
+
+    return array;
+  }
 }
 
 export { TruckModel };
